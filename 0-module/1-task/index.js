f6553c625d8f4f643ce40ae7e75f58d8eeba30dd
Mivich <mikhail.ivichev@mail.ru>
2023-01-17 22:42:56 +0300

    Сделал первую звдвяц

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sum(m, n) {
   // ваш код...
+  let result = m + n;
+  console.log(result);
 }
+sum(1,1);
+
+function calcSum(one = 2,two = 5) {
+  let sum = one + two;
+  console.log(sum);
+}
+calcSum();
