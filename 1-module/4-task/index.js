c636c1c913bc994b75d8b7c25d34e45fc431992e
Mivich <mikhail.ivichev@mail.ru>
2023-01-21 16:59:02 +0300

    Занятие 1: Основы JavaScript

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,8 @@
 function checkSpam(str) {
   // ваш код...
+  str = str.toString().toLowerCase();
+  return str.includes('xxx') || str.includes('1xbet') || false;
 }
+checkSpam('1XbeT now') === true
+checkSpam('free xxxxx') === true
+checkSpam('innocent rabbit') === false
