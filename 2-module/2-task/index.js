2df3a2f459fa6662b228354d4527124b4b34500a
Mivich <mikhail.ivichev@mail.ru>
2023-01-25 18:24:28 +0300

    Занятие 2: Объекты

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,14 @@
 function isEmpty(obj) {
   // ваш код...
+  for (let k in obj) {
+    return false;
+  }
+  return true;
 }
+let schedule = {};
+
+console.log(( isEmpty(schedule) )); 
+
+schedule["8:30"] = "подъём";
+
+console.log(( isEmpty(schedule) ));
